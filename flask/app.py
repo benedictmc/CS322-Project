d1368fbafd66165e8c6b55077823796ee817edc7
FRANCIS BENEDICT MARTIN MC GOVERN <u180201@AD.CS.MUCAMPUS.IE>
2018-10-25 14:57:33 +0100

    Linked homepage files with flask

--- flask/app.py
+++ flask/app.py
@@ -1,22 +1,10 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-artist_list = ['Kanye', 'The Beatles']
-
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello_world():
+    return render_template('HomePage.html')
 
 
-@app.route('/home')
-def home(): 
-
-    return render_template('home.html')
-
-@app.route('    ')
-def retrive_songs():
-    return str(artist_list)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+        app.run()
