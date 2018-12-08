17b6453f590449f04e53a722d4b0e1f6cdfdd386
Benedict McGovern <benmcgovern13@gmail.com>
2018-12-08 21:53:24 +0000

    Adding tpday changes to backend

--- use_model.py
+++ use_model.py
@@ -1,16 +1,19 @@
 from textgenrnn import textgenrnn
 from keras import backend as K
-def generate():
+def generate(artist):
     result = {}
-    textgen = textgenrnn(weights_path='models/genrnn/colaboratory_weights.hdf5',
-                        vocab_path='models/genrnn/colaboratory_vocab.json',
-                        config_path='models/genrnn/colaboratory_config.json')
+    textgen = textgenrnn(weights_path=f'models/genrnn/{artist}_weights.hdf5',
+                        vocab_path=f'models/genrnn/{artist}_vocab.json',
+                        config_path=f'models/genrnn/{artist}_config.json')
 
     textgen.generate_to_file('textgenrnn_texts.txt', max_gen_length=1000)
 
     with open('textgenrnn_texts.txt', 'r') as f:
         result['result'] = f.read()
-    
+    print(result)
     K.clear_session()
     
     return result
+
+
+generate('kanye')
