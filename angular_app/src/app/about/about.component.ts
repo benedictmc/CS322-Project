85803e6d498b2139dde7a461ddc81f334d82c794
FRANCIS BENEDICT MARTIN MC GOVERN <u180201@AD.CS.MUCAMPUS.IE>
2018-12-12 15:54:31 +0000

    Adding howler js

--- angular_app/src/app/about/about.component.ts
+++ angular_app/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Howl, Howler} from 'howler';
 
 @Component({
   selector: 'app-about',
@@ -10,6 +11,20 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  playSound(){
+    console.log("Button Clicked!")
+
+    let sound1 = new Howl({
+      src: ['../../assets/sound/adele.mp3']
+    });
+    let sound2 = new Howl({
+      src: ['../../assets/sound/adele-background.mp3']
+    });
+      sound1.play()
+      sound2.play()
+
   }
 
 }
